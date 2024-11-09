7ce30d12ae739f73903d016b7a295d29612fc4c7
yehuuu6 <therenaydin@gmail.com>
2024-11-09 19:25:45 +0300

    Enhance console functions: improve log clearing messages and add test command function

--- packages/console/functions.py
+++ packages/console/functions.py
@@ -3,6 +3,7 @@ Provides functions for the console package.
 """
 from packages.utils import printb
 from packages.console.command_registery import command_registry
+from typing import Any
 
 def help_function() -> None:
     print('Usage: python pulse.py <command> [args]')
@@ -12,11 +13,15 @@ def help_function() -> None:
 
 def clear_logs_function(file_name: str) -> None:
     try:
-        printb(f'Clearing {file_name}...', log=True)
+        printb(f'Clearing "{file_name}"...', log=True)
         # Check if the file exists
         open(f'logs/{file_name}', 'r').close()
         with open(f'logs/{file_name}', 'w') as f:
             f.write('')
         printb(f'{file_name} cleared successfully.', log=True)
     except Exception as e:
-        printb(f'Failed to clear {file_name}: {e}', log=True)
+        printb(f'Failed to clear given log file: {e}', log=True)
+
+def test_command_function(arg1: Any, arg2: Any) -> None:
+    args = [arg1, arg2]
+    print(f'Test command executed with args: {args}')
