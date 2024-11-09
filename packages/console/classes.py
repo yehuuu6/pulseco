3fbb5b474cccbadc641387a4d24e7ec9e0541267
yehuuu6 <therenaydin@gmail.com>
2024-11-09 19:25:10 +0300

    Fix TypeError handling & add usage for command

--- packages/console/classes.py
+++ packages/console/classes.py
@@ -4,10 +4,17 @@ class Command:
     """
     Class that represents a command that can be executed in the pulse console.
     """
-    def __init__(self, name: str, description: str, function: Callable[..., None]) -> None:
+    def __init__(self, name: str, description: str, usage: str, function: Callable[..., None]) -> None:
         self.name = name
         self.description = description
+        self.usage = usage
         self.function = function
+    
+    def __repr__(self) -> str:
+        return f'Command(name={self.name}, description={self.description}, usage={self.usage})'
 
     def execute(self, *args: Any, **kwargs: Any) -> None:
-        self.function(*args, **kwargs)
+        try:
+            self.function(*args, **kwargs)
+        except TypeError:
+            print(self.usage)
