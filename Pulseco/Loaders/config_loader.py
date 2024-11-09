4b5375899d9b1ede2316118cdc54215e6eb00467
Eren Aydın <therenaydin@gmail.com>
2024-11-10 01:20:16 +0300

    Refactor config handling to use ServerConfig class and improve default config loading

--- Pulseco/Loaders/config_loader.py
+++ Pulseco/Loaders/config_loader.py
@@ -2,24 +2,11 @@
 Provides config loader and validation functionality for the pulseco app.
 """
 
-from pydantic import BaseModel, PositiveInt
 from pulseco.utils import printb
+from pulseco.loaders.classes import ServerConfig
+from os import path, mkdir
 
-class Config(BaseModel):
-    host: str
-    port: PositiveInt
-    id: str
-    max_users: PositiveInt
-    name: str
-    description: str
-    public: bool
-    logging: str
-    enable_plugins: bool
-    render_muted_messages: bool
-    render_executed_commands: bool
-    lang: str
-
-DEFAULT_CONFIG: Config = Config(
+DEFAULT_CONFIG: ServerConfig = ServerConfig(
     host="127.0.0.1",
     port=7030,
     id="pulseco",
@@ -34,17 +21,33 @@ DEFAULT_CONFIG: Config = Config(
     lang="en"
 )
 
-def load_config() -> Config:
+def use_default_config() -> ServerConfig:
+    """
+    Saves the default config to settings.json and returns it.
+    """
+    if not path.exists("config"):
+        try:
+            mkdir("config")
+        except Exception as e:
+            printb(f"Failed to create config directory: {e}", log=True)
+            printb("Using default config from memory instead...", log=True)
+            return DEFAULT_CONFIG
+
+    printb("Using default config instead...", log=True)
+    with open("config/settings.json", "w") as f:
+        f.write(DEFAULT_CONFIG.model_dump_json(indent=4))
+    return DEFAULT_CONFIG
+
+def load_config() -> ServerConfig:
     """
     Loads the config from settings.json and validates it.
     """
     try:
         printb("Loading config...", log=True)
         with open("config/settings.json", "r") as f:
-            config = Config.model_validate_json(f.read(), strict=True)
+            config = ServerConfig.model_validate_json(f.read(), strict=True)
         printb("Config loaded successfully.", log=True)
     except Exception as e:
         printb(f"Failed to load config: {e}", log=True)
-        config = DEFAULT_CONFIG
-        printb("Using default config instead...", log=True)
+        config = use_default_config()
     return config
