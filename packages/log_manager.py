eeaed20f9c0cb1cf6a35885560620fc1859f887d
yehuuu6 <therenaydin@gmail.com>
2024-11-09 18:45:33 +0300

    Move folder and file checks to top

--- packages/log_manager.py
+++ packages/log_manager.py
@@ -6,15 +6,11 @@ from logging import getLogger, Logger, basicConfig, INFO
 Provides logging functionality for the pulseco app.
 """
 
-# Create the main logger
-main_logger: Logger = getLogger('pulseco') # Create the main logger
-basicConfig(filename='logs/server.log', level=INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
 # If logs directory does not exist, create it
 if not path.exists('logs'):
-    main_logger.info('Creating logs directory...')
+    print('Creating logs directory...')
     mkdir('logs')
-    main_logger.info('Logs directory created.')
+    print('Logs directory created.')
 
 # List of log files
 log_files: List[str] = ['chat.log', 'commands.log', 'server.log']
@@ -22,7 +18,12 @@ log_files: List[str] = ['chat.log', 'commands.log', 'server.log']
 # Create log files if they don't exist
 for log_file in log_files:
     if not path.exists(f'logs/{log_file}'):
-        main_logger.info(f'Creating {log_file}...')
+        print(f'Creating {log_file}...')
         with open(f'logs/{log_file}', 'w') as f:
             f.write('')
-        main_logger.info(f'{log_file} created.')
+        print(f'{log_file} created.')
+
+
+# Create the main logger
+main_logger: Logger = getLogger('pulseco') # Create the main logger
+basicConfig(filename='logs/server.log', level=INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
