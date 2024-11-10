ec688088aa82b78c32fc871f708b24f4a6bd905a
Eren Aydın <therenaydin@gmail.com>
2024-11-10 10:25:15 +0300

    Refactor Command class to use Pydantic for improved validation and documentation

--- Pulseco/Console/classes.py
+++ Pulseco/Console/classes.py
@@ -1,14 +1,22 @@
 from typing import Callable, Any
+from pydantic import BaseModel, Field
 
-class Command:
+class Command(BaseModel):
     """
     Class that represents a command that can be executed in the pulse console.
     """
-    def __init__(self, name: str, description: str, usage: str, function: Callable[..., None]) -> None:
-        self.name = name
-        self.description = description
-        self.usage = usage
-        self.function = function
+    name: str = Field(
+        title='Command name',
+        description='Name of the command to display and use.')
+    description: str = Field(
+        title='Command description',
+        description='Description of the command to display.')
+    usage: str = Field(
+        title='Command usage',
+        description='Usage of the command to help users execute it.')
+    function: Callable[..., None] = Field(
+        title='Command function',
+        description='Function that will be executed when the command is called.')
     
     def __repr__(self) -> str:
         return f'Command(name={self.name}, description={self.description}, usage={self.usage})'
