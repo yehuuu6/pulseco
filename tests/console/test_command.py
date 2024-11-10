624a30b68bdc53bccb2019ce1d85cbd0b68e0f54
yehuuu6 <therenaydin@gmail.com>
2024-11-10 15:05:02 +0300

    Format using black formatter

--- tests/console/test_command.py
+++ tests/console/test_command.py
@@ -2,60 +2,61 @@ from pulseco.console.classes import Command
 from io import StringIO
 from contextlib import redirect_stdout
 
+
 def mock_function():
-    print('Test command executed.')
+    print("Test command executed.")
+
 
 def test_command_init():
     """
     Test the initialization of a Command object.
     """
     test_class = Command(
-        name='test',
-        description='Test command',
-        usage='test',
-        function=mock_function)
-    assert test_class.name == 'test'
-    assert test_class.description == 'Test command'
-    assert test_class.usage == 'test'
+        name="test", description="Test command", usage="test", function=mock_function
+    )
+    assert test_class.name == "test"
+    assert test_class.description == "Test command"
+    assert test_class.usage == "test"
     assert test_class.function == mock_function
+
 
 def test_command_repr():
     """
     Test the __repr__ method of a Command object.
     """
     test_class = Command(
-        name='test',
-        description='Test command',
-        usage='test',
-        function=mock_function)
-    assert repr(test_class) == 'Command(name=test, description=Test command, usage=test)'
+        name="test", description="Test command", usage="test", function=mock_function
+    )
+    assert (
+        repr(test_class) == "Command(name=test, description=Test command, usage=test)"
+    )
+
 
 def test_command_execute():
     """
     Test the execute method of a Command object.
     """
     test_class = Command(
-        name='test',
-        description='Test command',
-        usage='test',
-        function=mock_function)
+        name="test", description="Test command", usage="test", function=mock_function
+    )
     test_class.execute()
+
 
 def test_command_to_raise_type_error_on_missing_args():
     """
     Test the execute method of a Command object with arguments.
     """
-    def test_function_with_args(arg1: str, arg2: str):
-        print(f'Test command needs these args: {arg1} and {arg2}.')
+
+    def test_function_with_args(arg1: str, arg2: str): ...
 
     test_class = Command(
-        name='test',
-        description='Test command',
-        usage='Usage: test <arg1> <arg2>',
-        function=test_function_with_args
+        name="test",
+        description="Test command",
+        usage="Usage: test <arg1> <arg2>",
+        function=test_function_with_args,
     )
-    
-    argv = ['pulse.py', 'test', 'arg1'] # Missing the second argument
+
+    argv = ["pulse.py", "test", "arg1"]  # Missing the second argument
 
     # Capture the output using a context manager
     captured_output = StringIO()
@@ -64,4 +65,4 @@ def test_command_to_raise_type_error_on_missing_args():
         test_class.execute(*argv[2:])
 
     # Check if the usage message was printed
-    assert captured_output.getvalue().strip() == 'Usage: test <arg1> <arg2>'
+    assert captured_output.getvalue().strip() == "Usage: test <arg1> <arg2>"
