3b744ea057033e7283f6ce36d73e180bb70140e1
Eren Aydın <therenaydin@gmail.com>
2024-11-11 22:05:53 +0300

    Refactor pulse.py to improve string formatting and code readability

--- pulse.py
+++ pulse.py
@@ -6,17 +6,18 @@ from sys import argv
 import pulseco.console.commands as Pulse
 from pulseco.console.command_registry import command_registry
 
+
 def main() -> None:
     """
     The main function of the pulseco command line tool.
     """
-    Pulse.init_module() # Initialize the console package to prevent circular imports error.
+    Pulse.init_module()
 
     if len(argv) == 1:
-        print('Usage: pulse.py <command> [args]')
+        print("Usage: pulse.py <command> [args]")
         print('Use "python pulse.py help" for help.')
         return
-    
+
     command = argv[1]
     # Get all the commands from the command registry and store them in a dictionary.
     commands = {cmd.name: cmd for cmd in command_registry.get_commands()}
@@ -24,8 +25,9 @@ def main() -> None:
     if command in commands:
         commands[command].execute(*argv[2:])
     else:
-        print(f'Unknown command: {command}')
+        print(f"Unknown command: {command}")
         print('Use "python pulse.py help" for help.')
+
 
 if __name__ == "__main__":
     main()
