36db674657f1fb1a80b01805205a1656b526bfb8
yehuuu6 <therenaydin@gmail.com>
2024-11-12 00:34:01 +0300

    Refactor log manager to create logs directory structure and update log file paths

--- pulseco/log_manager.py
+++ pulseco/log_manager.py
@@ -7,23 +7,19 @@ Provides logging functionality for the pulseco app.
 """
 
 # If logs directory does not exist, create it
-if not os.path.exists("logs"):
-    print("Creating logs directory...")
-    try:
-        os.mkdir("logs")
-        print("Logs directory created.")
-    except Exception as e:
-        print(f"Failed to create logs directory: {e}")
-        exit(1)
-
+try:
+    os.makedirs("app/logs", exist_ok=True)
+except Exception as e:
+    print(f"Failed to create logs directory: {e}")
+    exit(1)
 # List of log files
 log_files: List[str] = ["chat.log", "commands.log", "server.log"]
 
 # Create log files if they don't exist
 for log_file in log_files:
-    if not os.path.exists(f"logs/{log_file}"):
+    if not os.path.exists(f"app/logs/{log_file}"):
         print(f"Creating {log_file}...")
-        with open(f"logs/{log_file}", "w") as f:
+        with open(f"app/logs/{log_file}", "w") as f:
             f.write("")
         print(f"{log_file} created.")
 
@@ -31,7 +27,7 @@ for log_file in log_files:
 # Create the main logger
 main_logger: Logger = getLogger("pulseco")  # Create the main logger
 basicConfig(
-    filename="logs/server.log",
+    filename="app/logs/server.log",
     level=INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
